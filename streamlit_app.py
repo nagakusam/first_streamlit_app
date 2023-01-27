2a2ea7fec82885c8fc5d48c0e58559b3303c2d9a
nagakusam <123734056+nagakusam@users.noreply.github.com>
2023-01-27 16:41:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍗 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🥪 Avocado Toast')
+
+streamlit.header('🍌🍓 Build your own Fruit Smoothie 🥝🍇')
