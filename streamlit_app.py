33044568d1913bc455418f7ba6b9f4ebf77eebab
nagakusam <123734056+nagakusam@users.noreply.github.com>
2023-01-27 16:39:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
-streamlit.text('🍜Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍗 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🥪 Avocado Toast')
